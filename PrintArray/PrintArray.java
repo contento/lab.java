5a016aa420b3d3220b16e749dc98e9d144b91eab
Gonzalo Contento <gonzalo.contento@ericsson.com>
2018-03-19 11:05:38 -0400

    Fix algorithm

--- PrintArray/PrintArray.java
+++ PrintArray/PrintArray.java
@@ -8,15 +8,19 @@ public class PrintArray {
         printArrayAdvanced(array);
 
         for (int i = 0; i < n; i++) {
-            printArrayBasic(array, i);
+            printArrayBasic(array, i+1);
         }
     }
 
     static void printArrayAdvanced(int[] array) {
-        System.out.print(Arrays.toString(array));
+        System.out.println(Arrays.toString(array));
     }
 
     static void printArrayBasic(int[] array, int n) {
+        if (array == null || array.length == 0 || n <= 0)
+            return;
+
+        System.out.printf("[](%d) => ", n);
         for (int i = 0; i < n; i++) {
             System.out.print(array[i] + " ");
         }
