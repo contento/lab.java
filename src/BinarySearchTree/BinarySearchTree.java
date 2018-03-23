b081422e1f1ee605879ace12d6b2bb488793a1dd
Gonzalo Contento <gonzalo@conten.to>
2018-03-22 22:53:05 -0400

    Add search

--- src/BinarySearchTree/BinarySearchTree.java
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -2,30 +2,30 @@ package BinarySearchTree;
 
 import java.util.Arrays;
 
-import javax.crypto.NullCipher;
-
 public class BinarySearchTree {
     public static void main(String[] args) {
-        executeCase(new Integer[] { 12, 11, 13, 5, 6, 7 }, new Integer[] { 1956, 1963 }, new Integer[] { 6, 7 });
+        executeCase(new Integer[] { 12, 11, 13, 5, 6, 7 }, new Integer[] { 6, 7 });
+        executeCase(new String[] { "Foo", "Bar", "Foo", "Good" }, new String[] { "Bar" });
     }
 
-    private static <T extends Comparable<T>> void executeCase(final T[] initialValues, final T[] insertionValues,
-            final T[] deletionValues) {
-        printArray("Original: ", initialValues);
-
+    private static <T extends Comparable<T>> void executeCase(final T[] initialValues, final T[] deletionValues) {
         Node<T> root = null;
 
-        System.out.println("(.) Add");
+        printArray("Add: ", initialValues);
         root = addToTree(root, initialValues);
         printTreeInOrder(root);
 
-        System.out.println("(+) Add more");
-        root = addToTree(root, insertionValues);
-        printTreeInOrder(root);
-
-        System.out.println("(-) Remove some");
+        printArray("Remove: ", deletionValues);
         root = removeFromTree(root, deletionValues);
         printTreeInOrder(root);
+
+        T data = initialValues[0];
+        Node<T> node = search(root, data);
+        if (node != null) {
+            System.out.printf("Found [%s]%n", node.Data);
+        } else {
+            System.out.printf("Not found [%s]%n", data);
+        }
     }
 
     private static <T extends Comparable<T>> Node<T> addToTree(Node<T> root, T[] values) {
@@ -36,9 +36,19 @@ public class BinarySearchTree {
         return root;
     }
 
-
     private static <T extends Comparable<T>> Node<T> removeFromTree(Node<T> root, T[] values) {
         return root;
+    }
+
+    private static <T extends Comparable<T>> Node<T> search(Node<T> root, T data) {
+        if (root == null || root.eq(data)) {
+            return root;
+        }
+        if (root.gt(data)) {
+            return search(root.Left, data);
+        }
+
+        return search(root.Right, data);
     }
 
     private static <T extends Comparable<T>> Node<T> Insert(Node<T> root, T data) {
--- src/BinarySearchTree/Node.java
+++ src/BinarySearchTree/Node.java
@@ -11,9 +11,22 @@ public class Node<T extends Comparable<T>> {
         Left = Right = null;
     }
 
+    public boolean eq(T data) {
+        return Data.compareTo(data) == 0;
+    }
+
+    public boolean lt(T data) {
+        return Data.compareTo(data) < 0;
+    }
+
     public boolean le(T data) {
         return Data.compareTo(data) <= 0;
     }
+
+    public boolean gt(T data) {
+        return Data.compareTo(data) > 0;
+    }
+
     public boolean ge(T data) {
         return Data.compareTo(data) >= 0;
     }
